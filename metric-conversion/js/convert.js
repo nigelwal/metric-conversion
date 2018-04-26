68e67cda2350c2ce22acfe64aa2d98b75e83df79
nigelwal <35769273+nigelwal@users.noreply.github.com>
2018-04-26 16:00:02 +0100

    Added some more comments
    Added some more comments to explain variables related to calculations

--- metric-conversion/js/convert.js
+++ metric-conversion/js/convert.js
@@ -3,6 +3,7 @@ function fToM(){
 
   var measure = parseInt(document.getElementById("value1").value);
 
+  //This variable carries out the calculation for foot to miles
   var meters = measure * 0.3048;
   var message = measure + ' feet converts to ' + meters + ' meters.';
   console.log(message);
@@ -15,6 +16,7 @@ function inchesToCm(){
 
   var measure = parseInt(document.getElementById("value1").value);
 
+   //This variable carries out the calculation for inches to centimeters
   var centimeters = measure * 2.54;
   var message2 = measure + ' inches converts to ' + centimeters + ' centimeters.';
   console.log(message2);
@@ -27,6 +29,7 @@ function yardsToM(){
 
   var measure = parseInt(document.getElementById("value1").value);
 
+  //This variable carries out the calculation for yards to meters
   var yardmeters = measure * 0.9144;
   var message3 = measure + ' yard converts to ' + yardmeters + ' meters.';
   console.log(message3);
@@ -39,6 +42,7 @@ function MilesToKm(){
 
   var measure = parseInt(document.getElementById("value1").value);
 
+ //This variable carries out the calculation for miles to kilometers
   var km = measure * 1.60934;
   var message4 = measure + ' mile converts to ' + km + ' kilometers.';
   console.log(message4);
