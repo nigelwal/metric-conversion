ef052c1275784bdb9ba0be24d032bf31d2e489ce
nigelwal <35769273+nigelwal@users.noreply.github.com>
2018-04-26 15:33:48 +0100

    Added Buttons
    Buttons don't function correctly yet. Just added them before doing the javascript functionality

--- metric-conversion/js/convert.js
+++ metric-conversion/js/convert.js
@@ -8,3 +8,36 @@ function fToM(){
   document.getElementbyId("resultsentence").innerHTML = message;
 
 }
+
+function inchesToCm(){
+
+  var measure = parseInt(document.getElementById("value1").value);
+
+  var meters = measure * 0.3048;
+  var message2 = measure + ' feet converts to ' + meters + ' meters.';
+  console.log(message2);
+  document.getElementbyId("resultsentence").innerHTML = message2;
+
+}
+
+function yardsToM(){
+
+  var measure = parseInt(document.getElementById("value1").value);
+
+  var meters = measure * 0.3048;
+  var message3 = measure + ' feet converts to ' + meters + ' meters.';
+  console.log(message3);
+  document.getElementbyId("resultsentence").innerHTML = message3;
+
+}
+
+function MilesToKm(){
+
+  var measure = parseInt(document.getElementById("value1").value);
+
+  var meters = measure * 0.3048;
+  var message4 = measure + ' feet converts to ' + meters + ' meters.';
+  console.log(message4);
+  document.getElementbyId("resultsentence").innerHTML = message4;
+
+}
